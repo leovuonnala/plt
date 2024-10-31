d95b4b715a2a4b5422ba0bfb030e09f0d1d33ca4
Leo Vuonnala <leo.vuonnala@student.oulu.fi>
2024-10-31 17:04:27 +0200

    user story 3, test_word_start_with_vowel_end_with_y

--- piglatin.py
+++ piglatin.py
@@ -9,4 +9,17 @@ class PigLatin:
     def translate(self) -> str:
         if not self.phrase:
             return "nil"
+        words = self.phrase.split()
+        translated_words = []
+        for word in words:
+            if word[0] in 'aeiou':
+                translated_word = word + 'nay'
+            else:
+                first_vowel_idx = next((i for i, letter in enumerate(word) if letter in 'aeiou'), None)
+                if first_vowel_idx is not None:
+                    translated_word = word[first_vowel_idx:] + word[:first_vowel_idx] + 'ay'
+                else:
+                    translated_word = word + 'ay'
+            translated_words.append(translated_word)
+        return ' '.join(translated_words)
 
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -12,3 +12,7 @@ class TestPigLatin(unittest.TestCase):
     def test_empty_phrase(self):
         plt = PigLatin("")
         self.assertEqual("nil", plt.translate())
+
+    def test_word_start_with_vowel_end_with_y(self):
+        plt = PigLatin("any")
+        self.assertEqual("anynay", plt.translate())
